1759278dcdbd6f5fc33a42d96d1a774634d5eb26
Floreal Morandat florealm@gmail.com <florealm@gmail.com>
2016-09-11 09:45:30 +0000

    fixe basic sensors

--- grape.py
+++ grape.py
@@ -5,12 +5,13 @@ import logging
 
 
 BUSES = [0, 1] 
-INTERUPT_PIN = 7
+INTERUPT_PIN = 15
 
 _log = logging.getLogger(__name__)
 
 buses = {}
 stacks = {}
+display = None
 
 
 def _reverse_bits_of_byte(b):
@@ -18,36 +19,42 @@ def _reverse_bits_of_byte(b):
 
 class Stack(object):
 
-    def init(self, prefix):
+    def __init__(self, bus, prefix):
         self._prefix = prefix
+        self._bus = bus
         self.devices = {}
         for dev in STACK_DEVICES:
-            if dev.probe(prefix):
+            if dev.probe(bus, prefix):
                 self.devices[dev] = dev(bus, prefix)
-        for dev in devices:
+        for dev in self.devices.itervalues():
             try:
                 dev.setup()
             except Exception as e:
                 _log.error("Unable to setup %s: %s", dev, e)
+    def __getitem__(self, kind):
+        return self.devices[kind]
 
 class I2CDevice(object):
     CLASS_ADDRESS = 0x00 # Should be defined in each subclass
 
     def __init__(self, bus, prefix):
-        _bus = bus
-        _address = cls.make_address(prefix)
+        self._bus = bus
+        self._address = self.make_address(prefix)
+
+    def __str__(self):
+        return "{0}[0x{1}]".format(self.__class__.__name__, self._address)
 
     def setup(self):
         pass
 
     def read_byte(self):
-        self.bus.read_byte(self._address)
+        return self._bus.read_byte(self._address)
     def write_byte(self, value):
-        self.bus.read_byte(self._address, value)
+        self._bus.write_byte(self._address, value)
     def read_word(self, register):
-        self.bus.read_byte(self._address, register)
+        return self._bus.read_word_data(self._address, register)
     def write_word(self, register, value):
-        self.bus.read_byte(self._address, register, value)
+        self._bus.write_word_data(self._address, register, value)
 
     @classmethod
     def make_address(cls, prefix):
@@ -57,17 +64,14 @@ class I2CDevice(object):
     def probe(cls, bus, prefix):
         address = cls.make_address(prefix)
         try:
-            _log.debug("Probing %s at 0x%x.", cls, address)
             bus.read_byte(address)
-            _log.debug("Found %s at 0x%x.", cls, address)
             return True
         except:
-            _log.debug("%s not found at 0x%x.", cls, address)
             return False
         
 class PowerSwitch(I2CDevice):
     CLASS_ADDRESS = 0x38
-    L2P = [1, 2, 3, 4, 5, 6]
+    L2P = [1, 6, 3, 2, 5, 4]
 
     def setup(self):
         self.update()
@@ -77,34 +81,82 @@ class PowerSwitch(I2CDevice):
         self.write_byte(self._pin)
     def status(self):
         s = []
-        for i in range(len(L2P)):
+        for i in range(len(self.L2P)):
             if self[i]:
                 s.append(i)
+        return s
     def __getitem__(self, idx):
-        return self._pin & (1 << L2P[idx]) != 0
+        return self._pin & (1 << self.L2P[idx]) == 0
     def __setitem__(self, idx, state):
         if state:
-            self._pin |= (1 << L2P[idx])
+            self._pin &= ~(1 << self.L2P[idx])
         else:
-            self._pin &= ~(1 << L2P[idx])
-        self.update()
+            self._pin |= (1 << self.L2P[idx])
+        self.write()
+    def start_all(self):
+        for idx in range(len(self.L2P)):
+            self._pin &= ~(1 << self.L2P[idx])
+        self.write()
+    def stop_all(self):
+        for idx in range(len(self.L2P)):
+            self._pin |= 1 << self.L2P[idx]
+        self.write()
         
 
 class Temperature(I2CDevice):
     CLASS_ADDRESS = 0x40
     THM_REG = 0x00
 
-    def temperature(self):
-        data = self.read_word(self, self.THM_REG)
+    def get(self):
+        data = self.read_word(self.THM_REG)
         if data & 0x8000:
-            return - (~(data >> 8)) - (0.5 if data & 0x80 else 0)
+            return - (~((data >> 8) & 0x7F)) - (0.5 if data & 0x80 else 0)
         else:
             return (data >> 8) + (0.5 if data & 0x80 else 0)
 
 class PowerMeter(I2CDevice):
     CLASS_ADDRESS = 0x48
 
+    CALIBRATION_VALUE = 0x0666
+    CURRENT_DIVIDER = (1000 * 50)
+
+    CONF_REG = 0x00
+    CALIB_REG = 0x05
+    SHUNT_REG = 0x01
+    VOLTAGE_REG = 0x02
+    POWER_REG = 0x03
+    CURRENT_REG = 0x04
+
+    SHUNT_BUS_CONTINUOUS = 0x0007
+    SADCRES_12B_1S = 0x0018
+    BADCRES_12B = 0x0400
+    GAIN_320MV = 0x1800
+    GAIN2_80MV = 0x0800
+    BUS_VOLTAGE_RANGE_32V = 0x2000
+
+    def setup(self):
+        print("{0:x}".format(self.config_value()))
+        self.write_word(self.CONF_REG, self.config_value())
+        self.write_word(self.CALIB_REG, self.CALIBRATION_VALUE)
+
+    def config_value(self):
+        return self.SHUNT_BUS_CONTINUOUS \
+                | self.SADCRES_12B_1S \
+                | self.BADCRES_12B \
+                | self.GAIN2_80MV \
+                | self.BUS_VOLTAGE_RANGE_32V
+
+    def current(self): # in mA
+        return self.read_word(self.CURRENT_REG) / self.CURRENT_DIVIDER
+    def voltage(self): # in mV
+        return (self.read_word(self.VOLTAGE_REG) >> 3) * 4
+    def power(self): # in mW
+        return self.read_word(self.POWER_REG) / 2
+    def shunt(self):
+        return self.read_word(self.SHUNT_REG)
+
 class Display(I2CDevice):
+    CLASS_ADDRESS = 0x20
     RED = 0x01
     GREEN = 0x02
 
@@ -114,6 +166,9 @@ class Display(I2CDevice):
     SHORT_DELAY=0.001
 
     def setup(self):
+        # Interupt
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(INTERUPT_PIN, IN)
         #PCA init
         self._dev_write(0x04, [0x00])
         self._dev_write(0x05, [0x00])
@@ -171,12 +226,11 @@ class Display(I2CDevice):
     def _status(self):
         return self._dev_read(0x01) & 0x1C
 
-
 STACK_DEVICES = [PowerSwitch, Temperature, PowerMeter]
 
 for bus_id in BUSES:
     try:
-        _log.info("Probing SMBus %d.", bus_id)
+        _log.debug("Probing SMBus %d.", bus_id)
         bus = smbus.SMBus(bus_id)
         buses[bus_id] = bus
         _log.info("SMBus %d detected.", bus_id)
@@ -184,12 +238,17 @@ for bus_id in BUSES:
         _log.warn("SMBus %d not enabled.", bus_id)
 
 for bid, bus in buses.items():
+    _log.debug("Probing display on bus %d.", bid)
+    if Display.probe(bus, 0x07):
+        display = Display(bus, 0x07)
+        _log.info("Display found on bus %d.", bid)
+    else:
+        _log.debug("Display on bus %d not found.", bid)
     for sid in range(8):
         try:
-            _log.info("Probing stack %d on bus %d.", sid, bid)
+            _log.debug("Probing stack %d on bus %d.", sid, bid)
             bus.read_byte(PowerSwitch.make_address(sid))
             stacks[sid] = Stack(bus, sid)
             _log.info("stack %d on bus %d detected.", sid, bid)
         except Exception as e:
-            print(e)
-            _log.info("stack %d on bus %d not found.", sid, bid)
+            _log.debug("stack %d on bus %d not found.", sid, bid)
--- testgrape.py
+++ testgrape.py
@@ -2,4 +2,18 @@
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
-import grape
+from grape import *
+
+for i, s in stacks.items():
+    print("STACK: {0:o} {1}".format(i, s.devices))
+
+if False:
+    stacks[0][PowerSwitch].stop_all()
+    print(stacks[0][PowerSwitch].status())
+    stacks[0][PowerSwitch][0] = True
+    print(stacks[0][PowerSwitch].status())
+    print(stacks[0][Temperature].get())
+
+    print(stacks[0][PowerMeter].current())
+    print(stacks[0][PowerMeter].voltage())
+    print(stacks[0][PowerMeter].power())
