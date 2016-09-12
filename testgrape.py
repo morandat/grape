7f7c2b838c6576345a40706d5358d6fe419f3fc8
florealm@gmail.com <florealm@gmail.com>
2016-09-12 04:31:29 +0200

    Lcd works, but interrupts are still brokens

--- testgrape.py
+++ testgrape.py
@@ -21,12 +21,22 @@ if False:
 
 display.btn_handler = ButtonPrinter()
 
+display.print_str("Hello grape ... it's so fun to play with you")
+
 for i in range(5):
     display.led(Display.RED, i%2 == 1)
     display.led(Display.GREEN, i%2 == 0)
     sleep(.3)
 
+display.clear_screen()
 display.print_str("Hello grape ...")
+display.home(1)
+display.print_str("Bye")
+display.print_at(0, 0, "X")
+display.print_at(0, 15, "X")
+display.print_at(1, 0, "X")
+display.print_at(1, 15, "X")
+
 
 #GPIO.setup(4, GPIO.IN)
 #GPIO.setup(14, GPIO.IN)
